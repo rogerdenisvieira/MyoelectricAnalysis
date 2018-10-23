595f9df5be4e5adece9cf1bee73a4981a4dfd384
Roger Vieira <roger.vieira@agibank.com>
2018-10-23 20:18:41 -0200

    Calculating Shapiro, Cohen's D and value-p using series, not ratio between CVM/REP

--- analysis.R
+++ analysis.R
@@ -1,4 +1,4 @@
-df_selecionados <- read.csv(file = "/home/roger/Repositories/MyoelectricAnalysis/dados.csv", sep = ";", header = TRUE, dec = ".")
+df_selecionados <- read.csv(file = "C:/Users/roger.vieira/Repositories/MyoelectricAnalysis/dados.csv", sep = ";", header = TRUE, dec = ".")
 
 library("effsize")
 
@@ -6,40 +6,71 @@ library("effsize")
 #                                                   NORMALIDADE                                                    #
 ####################################################################################################################
 
-# Verificando normalidade da relação Repetição/CVM para cada músculo no período controle
-shapiro.test(df_selecionados$V37)
-shapiro.test(df_selecionados$V38)
-shapiro.test(df_selecionados$V39)
-shapiro.test(df_selecionados$V40)
+# CVM Controle
+shapiro.test(df_selecionados$V1)
+shapiro.test(df_selecionados$V2)
+shapiro.test(df_selecionados$V3)
+shapiro.test(df_selecionados$V4)
 
-# Verificando normalidade da relação Repetição/CVM para cada músculo na coleta inicial
-shapiro.test(df_selecionados$V41)
-shapiro.test(df_selecionados$V42)
-shapiro.test(df_selecionados$V43)
-shapiro.test(df_selecionados$V44)
+# Repeti��o Controle
+shapiro.test(df_selecionados$V5)
+shapiro.test(df_selecionados$V6)
+shapiro.test(df_selecionados$V7)
+shapiro.test(df_selecionados$V8)
 
-# Verificando normalidade da relação Repetição/CVM para cada músculo na coleta final
-shapiro.test(df_selecionados$V45)
-shapiro.test(df_selecionados$V46)
-shapiro.test(df_selecionados$V47)
-shapiro.test(df_selecionados$V48)
+# CVM Inicial
+shapiro.test(df_selecionados$V13)
+shapiro.test(df_selecionados$V14)
+shapiro.test(df_selecionados$V15)
+shapiro.test(df_selecionados$V16)
 
+# Repeticao Inicial
+shapiro.test(df_selecionados$V17)
+shapiro.test(df_selecionados$V18)
+shapiro.test(df_selecionados$V19)
+shapiro.test(df_selecionados$V20)
+
+# CVM Final
+shapiro.test(df_selecionados$V25)
+shapiro.test(df_selecionados$V26)
+shapiro.test(df_selecionados$V27)
+shapiro.test(df_selecionados$V28)
+
+# Repeticao Final
+shapiro.test(df_selecionados$V29)
+shapiro.test(df_selecionados$V30)
+shapiro.test(df_selecionados$V31)
+shapiro.test(df_selecionados$V32)
 
 ####################################################################################################################
-#                                           SIGNIFICÂNCIA - TEST T                                                 #
+#                                           SIGNIFICANCIA - TEST T                                                 #
 ####################################################################################################################
 
-# Relação Repetição/CVM de cada músculo entre controle e coleta inicial
-t.test(df_selecionados$V37, df_selecionados$V41) # Iliocostal direito
-t.test(df_selecionados$V38, df_selecionados$V42) # Longuíssimo direito
-t.test(df_selecionados$V39, df_selecionados$V43) # Iliocostal esquerdo
-t.test(df_selecionados$V40, df_selecionados$V44) # Longuíssimo esquerdo
 
-# Relação Repetição/CVM de cada músculo entre coleta inicial e coleta final
-t.test(df_selecionados$V41, df_selecionados$V45) # Iliocostal direito
-t.test(df_selecionados$V42, df_selecionados$V46) # Longuíssimo direito
-t.test(df_selecionados$V43, df_selecionados$V47) # Iliocostal esquerdo
-t.test(df_selecionados$V44, df_selecionados$V48) # Longuíssimo esquerdo
+# CVM Controle x Inicial
+t.test(df_selecionados$V1, df_selecionados$V13)
+t.test(df_selecionados$V2, df_selecionados$V14)
+t.test(df_selecionados$V3, df_selecionados$V15)
+t.test(df_selecionados$V4, df_selecionados$V16)
+
+# CVM Inicial x Final
+t.test(df_selecionados$V13, df_selecionados$V25)
+t.test(df_selecionados$V14, df_selecionados$V26)
+t.test(df_selecionados$V15, df_selecionados$V27)
+t.test(df_selecionados$V16, df_selecionados$V28)
+
+# Repeticao Controle x Inicial
+t.test(df_selecionados$V5, df_selecionados$V17)
+t.test(df_selecionados$V6, df_selecionados$V18)
+t.test(df_selecionados$V7, df_selecionados$V19)
+t.test(df_selecionados$V8, df_selecionados$V20)
+
+# Repeticao Inicial x Final
+t.test(df_selecionados$V17, df_selecionados$V29)
+t.test(df_selecionados$V18, df_selecionados$V30)
+t.test(df_selecionados$V19, df_selecionados$V31)
+t.test(df_selecionados$V20, df_selecionados$V32)
+
 
 # Dedos ao Solo - lado direito entre controle e coleta inicial
 t.test(df_selecionados$V11, df_selecionados$V23)
@@ -57,17 +88,30 @@ t.test(df_selecionados$V24, df_selecionados$V36)
 #                                      TAMANHO DO EFEITO - COHEN'S D                                               #
 ####################################################################################################################
 
-# d de Cohen para cada músculo entre o período controle e a coleta inical
-cohen.d(df_selecionados$V37, df_selecionados$V41)
-cohen.d(df_selecionados$V38, df_selecionados$V42)
-cohen.d(df_selecionados$V39, df_selecionados$V43)
-cohen.d(df_selecionados$V40, df_selecionados$V44)
+# CVM Controle x Inicial
+cohen.d(df_selecionados$V1, df_selecionados$V13)
+cohen.d(df_selecionados$V2, df_selecionados$V14)
+cohen.d(df_selecionados$V3, df_selecionados$V15)
+cohen.d(df_selecionados$V4, df_selecionados$V16)
 
-# d de Cohen para cada músculo entre a coleta inicial e a final
-cohen.d(df_selecionados$V41, df_selecionados$V45)
-cohen.d(df_selecionados$V42, df_selecionados$V46)
-cohen.d(df_selecionados$V43, df_selecionados$V47)
-cohen.d(df_selecionados$V44, df_selecionados$V48)
+# CVM Inicial x Final 
+cohen.d(df_selecionados$V13, df_selecionados$V25)
+cohen.d(df_selecionados$V14, df_selecionados$V26)
+cohen.d(df_selecionados$V15, df_selecionados$V27)
+cohen.d(df_selecionados$V16, df_selecionados$V28)
+
+# Repeticao Controle x Inicial
+cohen.d(df_selecionados$V5, df_selecionados$V17)
+cohen.d(df_selecionados$V6, df_selecionados$V18)
+cohen.d(df_selecionados$V7, df_selecionados$V19)
+cohen.d(df_selecionados$V8, df_selecionados$V20)
+
+# Repeticao Inicial x Final
+cohen.d(df_selecionados$V17, df_selecionados$V29)
+cohen.d(df_selecionados$V18, df_selecionados$V30)
+cohen.d(df_selecionados$V19, df_selecionados$V31)
+cohen.d(df_selecionados$V20, df_selecionados$V32)
+
 
 # Dedos ao Solo - lado direito entre controle e coleta inicial
 cohen.d(df_selecionados$V11, df_selecionados$V23)
@@ -80,3 +124,9 @@ cohen.d(df_selecionados$V23, df_selecionados$V35)
 
 # Dedos ao Solo - lado esquerdo entre coleta inicial e final
 cohen.d(df_selecionados$V24, df_selecionados$V36)
+
+
+
+
+
+
